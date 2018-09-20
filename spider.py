44754509bac16ff96469e55162e3301e9e0befd2
li-fstz <li-fstz@qq.com>
2018-09-20 20:35:26 +0800

    update

--- spider.py
+++ spider.py
@@ -30,7 +30,7 @@ def login (name, password, cookies = ''):
         else:
             return None
 
-def choice (s):
+def search (s):
     r = s.get ('http://222.27.192.13/WLSYJXGL/XSYW_XueShengDaiXuanShiYanXiangMU.aspx')
     soup = BeautifulSoup (r.text, "html.parser")
     XueQi_ID = soup.find ('input', id = 'hf_XueQi_ID')['value']
@@ -46,10 +46,8 @@ def choice (s):
         'rows': '20',
     }
     r = s.get ('http://222.27.192.13/WLSYJXGL/ajax/GetDaiXuanKeChengDt.aspx', data = data)
-
     d = json.loads (r.text)
     allData = []
-    cnt = 0
     for i in d['rows']:
         data = {
             'XM_ID': i['XM_ID'],
@@ -61,8 +59,7 @@ def choice (s):
         it = re.finditer (r'<b>指导教师：</b>(.{1,5})\s+.{30,50}<b>地点：</b>(.{10,40})\s+<b>时间：</b>(.{10,40})<(br|BR)></td><td>(\d+)</td><td>(\d+)</td><td>(\d+)</td><td>((<p style=\'color:red\'>已满</p>)|(<a.{70,80}【选课】</a>))', text)
         for match in it: 
             if (match.group(7) != match.group(5)):
-                cnt += 1
-                print (cnt, i['XM_MingCheng'])
+                print (len(allData) + 1, i['XM_MingCheng'])
                 print (match.group(1), match.group(2), match.group(3))
                 sch = re.search (r'XuanKeClick\((\d+),(\d+),(\d+),(\d+)\)', match.group(8))
                 data = {
@@ -73,47 +70,68 @@ def choice (s):
                     'xiaXian': sch.group(4),
                 }
                 allData.append (data)
-    if (cnt == 0):
+    if (len (allData) == 0):
         print ('暂时没有可选课程')
-    else:
-        id = int (input ("请输入序号：")) - 1
-        if (id > cnt - 1 or id < 0):
-            print ('输入错误')
-            exit ()
-        r = s.post ('http://222.27.192.13/WLSYJXGL/ajax/XuanKeByXS_ID.aspx', data=allData[id])
-        if (r.text == '1'):
-            print ('选课成功')
-        else:
-            print ('选课失败')
+        exit ()
+    return allData
 
-config = configparser.ConfigParser()
-config.read ('config.ini')
-s = None
-if (not 'spider' in config):
-    name = input ('请输入学号：')
-    password = getpass.getpass ('请输入密码：')
-    s = login (name, password)
-    if (s == None):
-        print ('学号或密码错误！')
-        exit ()
+def load (user = 'spider'):
+    config = configparser.ConfigParser()
+    config.read ('config.ini')
+    s = None
+    if (not user in config):
+        print ('新建账户 - %s' % user)
+        name = input ('请输入学号：')
+        password = getpass.getpass ('请输入密码：')
+        s = login (name, password)
+        if (s == None):
+            print ('学号或密码错误！')
+            exit ()
+        else:
+            config.read_dict ({user: {
+                                    'name': name, 
+                                    'password': password, 
+                                    'cookies': s.cookies['ASP.NET_SessionId'],
+                                }})
+            with open ('config.ini', 'w') as f:
+                config.write (f)
     else:
-        config.read_dict ({'spider': {
-                                'name': name, 
-                                'password': password, 
-                                'cookies': s.cookies['ASP.NET_SessionId'],
-                            }})
-        with open ('config.ini', 'w') as f:
-            config.write (f)
+        s = login (config[user]['name'], config[user]['password'], config[user]['cookies'])
+        if (s == None):
+            print ('学号或密码错误，%s 账户信息无效，以被删除！' % user)
+            config.remove_section (user)
+            with open ('config.ini', 'w') as f:
+                config.write (f)
+            exit ()
+        else:
+            config[user]['cookies'] = s.cookies['ASP.NET_SessionId']
+            with open ('config.ini', 'w') as f:
+                config.write (f)
+    return s
+
+def choice (s, data):
+    id = int (input ("请输入序号（输入0取消选课）："))
+    if (id > len(data) or id < 0):
+        print ('输入错误')
+        exit ()
+    elif (id == 0):
+        print ('取消选课')
+        exit ()
+    r = s.post ('http://222.27.192.13/WLSYJXGL/ajax/XuanKeByXS_ID.aspx', data = data[id - 1])
+    if (r.text == '1'):
+        print ('选课成功')
+    else:
+        print ('选课失败')
+
+if (len (sys.argv) == 1):
+    print ('查询模式 - 使用 spider 账户')
+    s = load ()
+    search (s)
 else:
-    s = login (config['spider']['name'], config['spider']['password'], config['spider']['cookies'])
-    if (s == None):
-        print ('学号或密码错误！')
-        config.remove_section ('spider')
-        with open ('config.ini', 'w') as f:
-            config.write (f)
-        exit ()
+    if (sys.argv[1] != 'spider'):
+        print ('选课模式 - 使用 %s 账户' % sys.argv[1])
+        s = load (sys.argv[1])
+        data = search (s)
+        choice (s, data)
     else:
-        config['spider']['cookies'] = s.cookies['ASP.NET_SessionId']
-        with open ('config.ini', 'w') as f:
-            config.write (f)
-choice (s)
+        print ('请不要使用 spider 账户选课！')
